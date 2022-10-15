39d10c081efa92ddebfd87e246e3703dcfdd818a
qn19325 <qn19325@bristol.ac.uk>
2022-10-15 15:42:54 +0100

    Add comments to JS files

--- draggableInventory.js
+++ draggableInventory.js
@@ -1,60 +1,48 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', (event) => { 
+  // addEventListener() - attaches an event handler to a document.
+  // 'DOMContentLoaded' the browser fully loaded HTML, and the DOM tree is built, but external resources like pictures <img> and stylesheets may not yet have loaded.
 
-    var dragSrcEl = null;
+    var sourceElement = null;
     
-    function handleDragStart(e) {
-      dragSrcEl = this;
-  
-      e.dataTransfer.effectAllowed = 'move';
-      e.dataTransfer.setData('text/html', this.innerHTML);
+    function startDrag(event) {
+      sourceElement = this;
+      // dataTransfer object is used to hold the data that is being dragged during a drag and drop operation.
+      // effectAllowed - provides all of the types of operations that are possible.
+      // setData - if data for the type doesn't exist it's added at the end, if data for the type already exists thr existing data is replaced in the same position.
+      // innerHTML - gets or sets the HTML or XML markup contained within the element.
+      event.dataTransfer.effectAllowed = 'move';
+      event.dataTransfer.setData('text/html', this.innerHTML);
     }
   
-    function handleDragOver(e) {
-      if (e.preventDefault) {
-        e.preventDefault();
+    function dragOver(event) {
+      if (event.preventDefault) {
+        // preventDefault() - tells the user agent that if the event doesn't get explicitly handled, its default action should not be taken as it normally would be.
+        event.preventDefault();
       }
-  
-      e.dataTransfer.dropEffect = 'move';
+      // dropEffect - Gets the type of drag-and-drop operation currently selected or sets the operation to a new type. 
+      event.dataTransfer.dropEffect = 'move';
       
       return false;
     }
   
-    function handleDragEnter(e) {
-      this.classList.add('over');
-    }
-  
-    function handleDragLeave(e) {
-      this.classList.remove('over');
-    }
-  
-    function handleDrop(e) {
-      if (e.stopPropagation) {
-        e.stopPropagation(); // stops the browser from redirecting.
+    function dropElement(event) {
+      if (event.stopPropagation) {
+        event.stopPropagation(); // stops the browser from redirecting.
       }
       
-      if (dragSrcEl != this) {
-        dragSrcEl.innerHTML = this.innerHTML;
-        this.innerHTML = e.dataTransfer.getData('text/html');
+      if (sourceElement != this) {
+        sourceElement.innerHTML = this.innerHTML;
+        this.innerHTML = event.dataTransfer.getData('text/html');
       }
       
       return false;
     }
-  
-    function handleDragEnd(e) {
-      
-      items.forEach(function (item) {
-        item.classList.remove('over');
-      });
-    }
     
-    
+    // querySelectorAll() - returns a static NodeList representing a list of the document's elements that match the specified group of selectors.
     let items = document.querySelectorAll('.container .box');
     items.forEach(function(item) {
-      item.addEventListener('dragstart', handleDragStart, false);
-      item.addEventListener('dragenter', handleDragEnter, false);
-      item.addEventListener('dragover', handleDragOver, false);
-      item.addEventListener('dragleave', handleDragLeave, false);
-      item.addEventListener('drop', handleDrop, false);
-      item.addEventListener('dragend', handleDragEnd, false);
+      item.addEventListener('dragstart', startDrag, false);
+      item.addEventListener('dragover', dragOver, false);
+      item.addEventListener('drop', dropElement, false);
     });
-  });
+});
